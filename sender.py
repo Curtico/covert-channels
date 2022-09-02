4ce836f654eab6af00ea8a7897a52eab78e801ce
Curtice Gough <31411535+Curtico@users.noreply.github.com>
2022-09-02 15:13:29 -0400

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,14 +1,14 @@
 from scapy.all import *
 from sys import argv
 
-if len(argv) == 1:
-    print("Usage: python3 sender.py <interface>")
+if len(argv) < 3:
+    print("Usage: python3 sender.py <interface> <message>")
     quit()
 
 conf.iface=argv[1]
 
 dst = "192.168.1."
-msg = "team poggers strikes again"
+msg = argv[2]
 
 for c in msg:
     send(ARP(pdst=dst+str(ord(c)), psrc='192.168.1.255'))
